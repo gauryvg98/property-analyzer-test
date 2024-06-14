4a293a5790e8a79177c90046f196300b9bd1c9df
Gaurboy's M3Pro <gaurboysm3pro@Yashvardhans-MacBook-Pro.local>
2024-06-14 22:22:43 +0530

    more fixes to the deployment process, because heroku is cheap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
@@ -29,11 +30,20 @@ app.include_router(visualization_router, prefix="/visualization")
 
 @app.get("/")
 def health():
+    load_data('./zoomprop_data_engineering.csv')
     return {"status" : "this service is healthy @ " + datetime.now().__str__()}
+
+@app.get("/load/")
+def load_data():
+    load_data('./zoomprop_data_engineering.csv')
+    return {"status" : "loaded data successfully"}
 
 if __name__ == "__main__":
     # initialize db before starting the api server
     sqlite_setup.create_db_tables()
     ## Load data
-    load_data('./zoomprop_data_engineering.csv')
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    #load_data('./zoomprop_data_engineering.csv')
+
+    # Use the PORT environment variable provided by Heroku
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
